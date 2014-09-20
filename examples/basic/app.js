5ae5022cf088f73d0e7713deda49cabb4182a281
Ryan Florence <rpflorence@gmail.com>
2014-09-20 08:32:44 -0600

    some CSS

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -4,6 +4,7 @@ var Modal = require('react-modal');
 var appElement = document.getElementById('example');
 
 Modal.setAppElement(appElement);
+Modal.injectCSS();
 
 var App = React.createClass({
 
@@ -17,15 +18,32 @@ var App = React.createClass({
     this.setState({modalIsOpen: !this.state.modalIsOpen});
   },
 
+  closeModal: function() {
+    this.setState({modalIsOpen: false});
+  },
+
   render: function() {
     return (
       <div>
         <button onClick={this.openModal}>Open Modal</button>
-        <Modal isOpen={this.state.modalIsOpen}>
+        <Modal
+          closeTimeoutMS={150}
+          dismissable={true}
+          isOpen={this.state.modalIsOpen}
+          onClose={this.closeModal}
+        >
           <h1>Hello</h1>
           <div>I am a modal</div>
           <form>
             <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <br/>
+            <button>hi</button>
+            <button>hi</button>
+            <button>hi</button>
             <button>hi</button>
           </form>
         </Modal>
