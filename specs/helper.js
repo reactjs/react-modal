10fe4052e4732b9b1cdaae5a28db0bfd7ebbb824
Ryan Florence <rpflorence@gmail.com>
2014-09-24 11:09:31 -0600

    getting ready for first publish

--- specs/helper.js
+++ specs/helper.js
@@ -3,25 +3,3 @@ expect = require('expect');
 React = require('react/addons');
 ReactTestUtils = React.addons.TestUtils;
 
-refute = function (condition, message) {
-  assert(!condition, message);
-};
-
-var RouteStore = require('../modules/stores/RouteStore');
-
-beforeEach(function () {
-  RouteStore.unregisterAllRoutes();
-});
-
-var transitionTo = require('../modules/actions/LocationActions').transitionTo;
-var MemoryLocation = require('../modules/locations/MemoryLocation');
-var PathStore = require('../modules/stores/PathStore');
-
-beforeEach(function () {
-  PathStore.setup(MemoryLocation);
-  transitionTo('/');
-});
-
-afterEach(function () {
-  PathStore.teardown();
-});
