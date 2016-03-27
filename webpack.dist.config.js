72c849807da9ea3fc0d95c1092ff5acd8dd8b4a5
Clay Diffrient <cdiffrient@instructure.com>
2016-03-26 23:18:36 -0600

    Move to using webpack for building the library
    This allows us to remove the dependency on browserify
    completely.

---
+++ webpack.dist.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var env = process.env.WEBPACK_ENV;
+
+module.exports = {
+
+  entry: {
+    'react-modal': './lib/index.js',
+    'react-modal.min': './lib/index.js'
+  },
+
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    path: 'dist',
+    publicPath: '/',
+    libraryTarget: 'umd',
+    library: 'ReactModal'
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.js?$/, exclude: /node_modules/, loader: 'babel'},
+    ]
+  }
+
+};
