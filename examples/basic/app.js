aa66819f45f62f222a0291571fd83fc4add33a04
Clay Diffrient <clay.diffrient@gmail.com>
2016-04-23 13:07:39 -0600

    [fixed] Removes unneeded sanitizeProps function (#169)
    fixes #165
    
    The sanitize props function seems to be unneeded and there
    is no concrete explanation for why it is there.  This removes
    it to prevent problems caused by calling it.

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -6,44 +6,14 @@ var appElement = document.getElementById('example');
 
 Modal.setAppElement('#example');
 
-Modal.defaultStyles = {
-  overlay : {
-    position          : 'fixed',
-    top               : 0,
-    left              : 0,
-    right             : 0,
-    bottom            : 0,
-    backgroundColor   : 'rebeccapurple'
-  },
-  content : {
-    position                   : 'absolute',
-    top                        : '40px',
-    left                       : '40px',
-    right                      : '40px',
-    bottom                     : '40px',
-    border                     : '1px solid #ccc',
-    background                 : '#fff',
-    overflow                   : 'auto',
-    WebkitOverflowScrolling    : 'touch',
-    borderRadius               : '4px',
-    outline                    : 'none',
-    padding                    : '20px'
-
-  }
-}
-
 var App = React.createClass({
 
   getInitialState: function() {
-    return { modalIsOpen: false, modalIsOpen2: false };
+    return { modalIsOpen: false };
   },
 
   openModal: function() {
     this.setState({modalIsOpen: true});
-  },
-
-  openModal2: function() {
-    this.setState({modalIsOpen2: true});
   },
 
   closeModal: function() {
@@ -69,7 +39,6 @@ var App = React.createClass({
     return (
       <div>
         <button onClick={this.openModal}>Open Modal</button>
-        <button onClick={this.openModal2}>Open Modal 2</button>
         <Modal
           ref="mymodal"
           closeTimeoutMS={150}
@@ -77,27 +46,6 @@ var App = React.createClass({
           onAfterOpen={this.handleOnAfterOpenModal}
           onRequestClose={this.handleModalCloseRequest}>
           <h1 ref="title">Hello</h1>
-          <button onClick={this.closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input onChange={this.handleInputChange} />
-            <input />
-            <input />
-            <input />
-            <input />
-            <br/>
-            <button>hi</button>
-            <button>hi</button>
-            <button>hi</button>
-            <button>hi</button>
-          </form>
-        </Modal>
-        <Modal
-          closeTimeoutMS={150}
-          isOpen={this.state.modalIsOpen2}
-          onRequestClose={this.handleModalCloseRequest}
-        >
-          <h1>Hello</h1>
           <button onClick={this.closeModal}>close</button>
           <div>I am a modal</div>
           <form>
