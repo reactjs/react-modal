c6dffa302d375080e4d15366f8b1f9f64dbc0f6a
michaseel <seelmichael@gmail.com>
2016-05-10 17:51:29 +0200

    feat(customClasses): allow animation with customClasses (--after-open and --before-close)

--- specs/Modal.spec.js
+++ specs/Modal.spec.js
@@ -143,6 +143,13 @@ describe('Modal', function () {
     equal(modal.portal.refs.overlay.style.backgroundColor, '');
   });
 
+  it('adds --after-open for animations when a custom ClassNames are used', function() {
+    var modal = renderModal({isOpen: true, className: 'myClass', overlayClassName: 'myOverlayClass'});
+    notEqual(modal.portal.refs.overlay.className.indexOf('myOverlayClass--after-open'), -1);
+    notEqual(modal.portal.refs.content.className.indexOf('myClass--after-open'), -1);
+    unmountModal();
+  });
+
   it('supports adding style to the modal contents', function () {
     var modal = renderModal({isOpen: true, style: {content: {width: '20px'}}});
     equal(modal.portal.refs.content.style.width, '20px');
