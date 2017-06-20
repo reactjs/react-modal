648cc2f08375808a29ee020edb8d3e0e10e81dd4
Bruno Dias <dias.h.bruno@gmail.com>
2017-06-19 23:47:14 -0300

    [fixed] update script path on bootstrap example.

--- examples/bootstrap/app.js
+++ examples/bootstrap/app.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Modal = require('../../lib/index');
+var Modal = require('../../src/index');
 
 var appElement = document.getElementById('example');
 
