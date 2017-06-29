f386aa424c8ae95da94b76238e277df4ca66cb5f
Bruno Dias <dias.h.bruno@gmail.com>
2017-06-28 21:28:26 -0300

    [chore] added more examples.

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -1,88 +1,36 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Modal from '../../src/index';
+import ViewA from './view_a';
+import ViewB from './view_b';
 
 const appElement = document.getElementById('example');
 
 Modal.setAppElement('#example');
 
+const heading = firstView => {
+  if (firstView) {
+    return "#1. Working with one modal at a time.";
+  }
+  return "#2. Working with many modal.";
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { modal1: false, modal2: false };
+    this.state = { firstView: true };
   }
 
-  toggleModal_1 = () => {
-    this.setState({ ...this.state, modal1: !this.state.modal1 });
-  }
-
-  toggleModal_2 = event => {
-    event.preventDefault();
-    this.setState({ ...this.state, modal2: !this.state.modal2 });
-  }
-
-  handleModalCloseRequest = () => {
-    // opportunity to validate something and keep the modal open even if it
-    // requested to be closed
-    this.setState({ ...this.state, modal1: false });
-  }
-
-  handleInputChange = () => {
-    this.setState({ ...this.state, foo: 'bar' });
-  }
-
-  handleOnAfterOpenModal = () => {
-    // when ready, we can access the available refs.
-    this.refs.title.style.color = '#F00';
+  toggleView = () => {
+    this.setState({ ...this.state, firstView: !this.state.firstView });
   }
 
   render() {
-    const { modal1, modal2 } = this.state;
     return (
       <div>
-        <button onClick={this.toggleModal_1}>Open Modal A</button>
-        <button onClick={this.toggleModal_2}>Open Modal B</button>
-        <Modal
-          ref="mymodal"
-          id="test"
-          closeTimeoutMS={150}
-          isOpen={modal1}
-          contentLabel="modalA"
-          onAfterOpen={this.handleOnAfterOpenModal}
-          onRequestClose={this.handleModalCloseRequest}>
-          <h1 ref="title">Hello</h1>
-          <button onClick={this.toggleModal_1}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input onChange={this.handleInputChange} />
-            <input />
-            <input />
-            <input />
-            <input />
-            <br/>
-            <button>hi</button>
-            <button>hi</button>
-            <button>hi</button>
-            <button>hi</button>
-            <button onClick={this.toggleModal_2}>Open Modal B</button>
-          </form>
-        </Modal>
-        <Modal ref="mymodal2"
-               id="test2"
-               aria={{
-                 labelledby: "heading",
-                 describedby: "fulldescription"
-               }}
-               closeTimeoutMS={150}
-               contentLabel="modalB"
-               isOpen={modal2}
-               onAfterOpen={() => {}}
-               onRequestClose={this.toggleModal_2}>
-          <h1 id="heading">This is the modal 2!</h1>
-          <div id="fulldescription" tabIndex="0" role="document">
-            <p>This is a description of what it does: nothing :)</p>
-          </div>
-        </Modal>
+        <button onClick={this.toggleView}>Click to go to the next example!</button>
+        <h2>{heading(this.state.firstView)}</h2>
+        {this.state.firstView ? <ViewA /> : <ViewB />}
       </div>
     );
   }
