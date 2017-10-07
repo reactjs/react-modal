b3701f681570505d332c465fd3ab1bc6a3d2ea65
Kenny Kaye <kenny@kaye.us>
2017-10-07 15:25:43 -0700

    [fixed] Issue #526 Import PropTypes as default import.

--- src/components/ModalPortal.js
+++ src/components/ModalPortal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
 import * as focusManager from '../helpers/focusManager';
 import scopeTab from '../helpers/scopeTab';
 import * as ariaAppHider from '../helpers/ariaAppHider';
