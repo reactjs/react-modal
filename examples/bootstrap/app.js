6147fe6fb2016c5eed3924de3f55c992099e184f
Nate Radebaugh <Nate.Radebaugh@swc.com>
2018-01-24 07:38:04 -0600

    [chore] Bump bootstrap example to use 4.0 release

--- examples/bootstrap/app.js
+++ examples/bootstrap/app.js
@@ -42,11 +42,11 @@ class App extends Component {
         >
           <div className="modal-content">
             <div className="modal-header">
+              <h4 className="modal-title">Modal title</h4>
               <button type="button" className="close" onClick={this.handleModalCloseRequest}>
                 <span aria-hidden="true">&times;</span>
                 <span className="sr-only">Close</span>
               </button>
-              <h4 className="modal-title">Modal title</h4>
             </div>
             <div className="modal-body">
               <h4>Really long content...</h4>
@@ -55,7 +55,7 @@ class App extends Component {
               <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-default" onClick={this.handleModalCloseRequest}>Close</button>
+              <button type="button" className="btn btn-secondary" onClick={this.handleModalCloseRequest}>Close</button>
               <button type="button" className="btn btn-primary" onClick={this.handleSaveClicked}>Save changes</button>
             </div>
           </div>
