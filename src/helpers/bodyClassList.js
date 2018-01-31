6c4d4ad919af198943e39325e16f35d27e980b73
Todd Kloots <tkloots@slack-corp.com>
2018-01-30 19:36:39 -0800

    [fixed] management of aria-hidden attribute decoupled from the management of the body open class

--- src/helpers/bodyClassList.js
+++ src/helpers/bodyClassList.js
@@ -1,20 +1,35 @@
-import * as refCount from "./refCount";
+const classListMap = {};
 
-export function add(bodyClass) {
-  // Increment class(es) on refCount tracker and add class(es) to body
+const addClassToMap = className => {
+  // Set variable and default if none
+  if (!classListMap[className]) {
+    classListMap[className] = 0;
+  }
+  classListMap[className] += 1;
+  return className;
+};
+
+const removeClassFromMap = className => {
+  if (classListMap[className]) {
+    classListMap[className] -= 1;
+  }
+  return className;
+};
+
+const add = bodyClass => {
   bodyClass
     .split(" ")
-    .map(refCount.add)
+    .map(addClassToMap)
     .forEach(className => document.body.classList.add(className));
-}
+};
 
-export function remove(bodyClass) {
-  const classListMap = refCount.get();
-  // Decrement class(es) from the refCount tracker
-  // and remove unused class(es) from body
+const remove = bodyClass => {
+  // Remove unused class(es) from body
   bodyClass
     .split(" ")
-    .map(refCount.remove)
+    .map(removeClassFromMap)
     .filter(className => classListMap[className] === 0)
     .forEach(className => document.body.classList.remove(className));
-}
+};
+
+export { add, remove };
