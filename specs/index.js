6a67946ea20bffec2012ef47cd4c84c9a8d2e763
Mike Deverell <mike@onepeloton.com>
2018-10-15 13:59:00 -0400

    add testability tests

--- specs/index.js
+++ specs/index.js
@@ -4,8 +4,10 @@ import ModalState from "./Modal.spec";
 import ModalEvents from "./Modal.events.spec";
 import ModalStyle from "./Modal.style.spec";
 import ModalHelpers from "./Modal.helpers.spec";
+import ModalTestability from "./Modal.testability.spec";
 
 describe("State", ModalState);
 describe("Style", ModalStyle);
 describe("Events", ModalEvents);
 describe("Helpers", ModalHelpers);
+describe("Testability", ModalTestability);
