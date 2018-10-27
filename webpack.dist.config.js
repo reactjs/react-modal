2f9000e2d3af92e829f05f592ae5a5a93f341a7c
Bruno Dias <dias.h.bruno@gmail.com>
2018-10-27 13:40:37 -0300

    [chore] update webpack to version 4 and refactor some configurations.

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,14 +1,14 @@
-var webpack = require('webpack');
-var path = require('path');
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+/* global require, __dirname, module, process */
+const webpack = require("webpack");
+const path = require("path");
 
-var reactExternal = {
+const reactExternal = {
   root: 'React',
   commonjs2: 'react',
   commonjs: 'react',
   amd: 'react'
 };
-var reactDOMExternal = {
+const reactDOMExternal = {
   root: 'ReactDOM',
   commonjs2: 'react-dom',
   commonjs: 'react-dom',
@@ -16,43 +16,34 @@ var reactDOMExternal = {
 };
 
 module.exports = {
-
+  mode: "production",
   entry: {
-    'react-modal': './src/index.js',
-    'react-modal.min': './src/index.js'
+    "react-modal": "./src/index.js",
+    "react-modal.min": "./src/index.js"
   },
-
   externals: {
-    'react': reactExternal,
-    'react-dom': reactDOMExternal
+    "react": reactExternal,
+    "react-dom": reactDOMExternal
   },
-
   output: {
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    libraryTarget: 'umd',
-    library: 'ReactModal'
+    filename: "[name].js",
+    chunkFilename: "[id].chunk.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    libraryTarget: "umd",
+    library: "ReactModal"
   },
-
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true,
-      compress: {
-        warnings: false
-      }
+      "process.env.NODE_ENV": JSON.stringify("production")
     })
   ],
-
   module: {
     rules: [
-      { test: /\.js?$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
+      { test: /\.js?$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" }
+      }
     ]
   }
-
 };
