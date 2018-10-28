8b51a2bd1f7eccb7c3516e7c5d22c552b7105b09
Bruno Dias <dias.h.bruno@gmail.com>
2018-10-28 10:05:38 -0300

    [chore] update dependencies and refactor configurations.
    PR #711.
    
    * [chore] update dependencies.
    * [chore] update webpack to version 4 and refactor some configurations.
    * [chore] update babel to 7 and clean up configurations.
    * [chore] update karma package and configuration.

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,15 +1,20 @@
-const path = require('path');
-const commonConfig = require('./webpack.config');
+/* global require, __dirname, module */
+const path = require("path");
+const commonConfig = require("./webpack.config");
 
+commonConfig.mode = "development";
+commonConfig.entry = "./specs/index.js";
+commonConfig.devtool = "inline-source-map";
 commonConfig.plugins = [];
-commonConfig.entry = './specs/index.js';
-commonConfig.devtool = 'inline-source-map';
 
 commonConfig.module.rules.unshift({
   test: /\.js$/,
-  use: { loader: 'istanbul-instrumenter-loader' },
-  enforce: 'post',
-  include: path.resolve(__dirname, './src')
+  use: {
+    loader: "istanbul-instrumenter-loader",
+    options: { esModules: true }
+  },
+  enforce: "post",
+  include: path.resolve(__dirname, "./src")
 });
 
 module.exports = commonConfig;
