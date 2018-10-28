6c2cdf7ae655661a51ceb2e5f506f0eff0ef9b8c
Bruno Dias <dias.h.bruno@gmail.com>
2018-10-28 10:05:38 -0300

    [chore] update dependencies and refactor configurations.

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,5 @@
+/* global require, process, module */
+
 let browsers = ['ChromeHeadless'];
 let coverageType = 'text';
 
@@ -6,45 +8,34 @@ if (process.env.CONTINUOUS_INTEGRATION) {
   coverageType = 'lcovonly';
 }
 
+// NOTE:
+// Increase `captureTimeout: number` to prevent the issue
+// with disconnected tests (https://goo.gl/nstA69).
+
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha'],
-
     preprocessors: {
       './src/*.js': ['coverage'],
       './src/**/*.js': ['coverage'],
       './specs/index.js': ['webpack', 'sourcemap']
     },
-
     files: ['./specs/index.js'],
-
     webpack: require('./webpack.test.config'),
-
     webpackMiddleware: { stats: 'errors-only' },
-
     reporters: ['mocha', 'coverage'],
-
     mochaReporter: { showDiff: true },
-
     coverageReporter: {
       type : coverageType,
       dir : 'coverage/',
       subdir: '.'
     },
-
     port: 9876,
-
     colors: true,
-
     logLevel: config.LOG_INFO,
-
     autoWatch: true,
-
     browsers,
-
-    // Increase timeouts to prevent the issue with disconnected tests (https://goo.gl/nstA69)
     captureTimeout: 4 * 60 * 1000,
-
-    singleRun: (process.env.CONTINUOUS_INTEGRATION)
+    singleRun: process.env.CONTINUOUS_INTEGRATION
   });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,49 @@
-var fs = require('fs');
-var path = require('path');
-var webpack = require('webpack');
+/* global require, __dirname, module, process */
+const fs = require("fs");
+const path = require("path");
+const webpack = require("webpack");
 
-var EXAMPLES_DIR = path.resolve(__dirname, 'examples');
+const BUILDING_MODE = process.env.NODE_ENV || "development";
+const EXAMPLES_DIR = path.resolve(__dirname, "examples");
 
-function isDirectory(dir) {
-  return fs.lstatSync(dir).isDirectory();
-}
+const isDirectory = dir => fs.lstatSync(dir).isDirectory();
 
-function buildEntries() {
-  return fs.readdirSync(EXAMPLES_DIR).reduce(function (entries, dir) {
-    if (dir === 'build')
-      return entries;
-
-    var isDraft = dir.charAt(0) === '_';
-
-    if (!isDraft && isDirectory(path.join(EXAMPLES_DIR, dir)))
-      entries[dir] = path.join(EXAMPLES_DIR, dir, 'app.js');
-
+const buildEntries = rootPath => fs.readdirSync(
+  rootPath
+).reduce((entries, dir) => {
+  if (dir === "build")
     return entries;
-  }, {});
-}
 
-module.exports = {
+  var isDraft = dir.charAt(0) === "_";
 
-  entry: buildEntries(),
+  if (!isDraft && isDirectory(path.join(rootPath, dir)))
+    entries[dir] = path.join(rootPath, dir, "app.js");
 
+  return entries;
+}, {});
+
+const configuration = {
+  mode: BUILDING_MODE,
+  entry: buildEntries(EXAMPLES_DIR),
   output: {
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js',
-    path: path.resolve(__dirname, './examples/__build__'),
-    publicPath: '/__build__/'
+    filename: "[name].js",
+    chunkFilename: "[id].chunk.js",
+    path: path.resolve(__dirname, "./examples/__build__"),
+    publicPath: "/__build__/"
   },
-
+  devtool: 'inline-source-map',
+  devServer: { contentBase: "./examples" },
   module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
-    ]
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: { loader: "babel-loader" }
+    }]
   },
-
   resolve: {
-    alias: {
-      "react-modal": path.resolve(__dirname, "./src")
-    }
+    alias: { "react-modal": path.resolve(__dirname, "./src") }
   },
-
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('shared'),
-    new webpack.LoaderOptionsPlugin({ debug: true })
-  ]
-
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })]
 };
+
+module.exports = configuration;
